d817f536887b282f56d217e77cd0b0f581dec93b
saidisko <saidd.iskender@gmail.com>
2025-12-04 16:41:24 +0300

    Title

--- student_grade_manager.cpp
+++ student_grade_manager.cpp
@@ -4,13 +4,13 @@ using namespace std;
 //student grade manager assignment 
 
 #define title_height 5
-#define title_width 10
+#define title_width 22
 string title = "STUDENT GRADE MANAGER"; 
 
 
 int main() {
-    for(int j = 0; j < 5; j++){
-        for(int i = 0; i < 22; i++){
+    for(int j = 0; j < title_height; j++){
+        for(int i = 0; i < title_width; i++){
             if(j == 0 || j == 4){
                 cout<<"-";
             }
