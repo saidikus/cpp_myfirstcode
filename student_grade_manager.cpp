bae77c8846d0361714313822e3cc28b4931e2fdb
saidisko <saidd.iskender@gmail.com>
2025-12-04 16:38:28 +0300

    Title

--- student_grade_manager.cpp
+++ student_grade_manager.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 using namespace std;
+
 //student grade manager assignment 
+
+#define title_height 5
+#define title_width 10
+string title = "STUDENT GRADE MANAGER"; 
+
+
 int main() {
-    cout << "hello motot!" << endl;
-    return 0;
-}
+    for(int j = 0; j < 5; j++){
+        for(int i = 0; i < 22; i++){
+            if(j == 0 || j == 4){
+                cout<<"-";
+            }
+            else if(j == 2 && i == 0){
+                cout<<title; 
+                i = i + (title.length() - 1); // place i after the title(skip it)
+            }
+            else
+                cout<<"  ";
+        }
+        cout<<endl; 
+    }
+}                                              
